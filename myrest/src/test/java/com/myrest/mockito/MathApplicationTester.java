460fb4cc2ad6ba2e4d3b50e79ab61b888416bb31
Paul Hsu <Paul Hsu@TORAHML9FPMQ72.am.mds.honda.com>
2016-10-27 12:05:24 -0700

    cc

--- myrest/src/test/java/com/myrest/mockito/MathApplicationTester.java
+++ myrest/src/test/java/com/myrest/mockito/MathApplicationTester.java
@@ -1,12 +1,19 @@
 package com.myrest.mockito;
 
+import java.util.Iterator;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import static org.mockito.Mockito.when;
+
+
+
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 // @RunWith attaches a runner with the test class to initialize the test data
 @RunWith(MockitoJUnitRunner.class)
@@ -27,5 +34,16 @@ public class MathApplicationTester {
 
       //test the add functionality
       Assert.assertEquals(mathApplication.add(10.0, 20.0),30.0,0);
+   }
+   
+   @Test
+   public void iterator_will_return_hello_world(){
+    //arrange
+    Iterator i=(Iterator) mock(Iterator.class);
+    when(i.next()).thenReturn("Hello").thenReturn("World");
+    //act
+    String result=i.next()+" "+i.next();
+    //assert
+    assertEquals("Hello World", result);
    }
 }
